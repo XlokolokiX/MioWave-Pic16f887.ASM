230cafa861374042177690b8e402435d54990a5f
XlokolokiX <60261460+XlokolokiX@users.noreply.github.com>
2024-05-19 19:26:42 -0300

    Add files via upload

---
+++ build/default/debug/main.o.d
@@ -0,0 +1,3 @@
+build/default/debug/main.o:  \
+	C:/Users/Mouton-PC/Desktop/Digital2/PROYECTO/main.asm  \
+
---
+++ build/default/production/main.o.d
@@ -0,0 +1,3 @@
+build/default/production/main.o:  \
+	C:/Users/Mouton-PC/Desktop/Digital2/PROYECTO/main.asm  \
+
